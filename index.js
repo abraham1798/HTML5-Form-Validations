35d7159d5b50e25b054cbb9f67c47c8deeba0fb3
abraham1798 <abrahamc.a@hotmail.com>
2021-03-04 21:16:30 +0000

    Trabajo Finalizado

---
+++ index.js
@@ -0,0 +1,26 @@
+let card = document.getElementById("cardinput");
+let cvc = document.getElementById("cvcinput");
+let money = document.getElementById("amountinput");
+let name = document.getElementById("nameinput");
+let lastName = document.getElementById("lastinput");
+let city = document.getElementById("cityinput");
+
+function ValidateInputs(myInput) {
+
+    if (myInput.value != "") {
+
+        return true;
+    } else {
+
+        return false;
+
+    }
+}
+function Validate() {
+    if (ValidateInputs(card) == true && ValidateInputs(cvc) == true && ValidateInputs(money) == true && ValidateInputs(name) == true && ValidateInputs(lastName) == true && ValidateInputs(city) == true) {
+        alert("Se ha llenado correctamente");
+
+    } else {
+        alert("Faltan espacios por rellenar");
+    }
+}
